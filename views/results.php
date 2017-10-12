19defde2650da34447e55438363908df15caf689
mrm80612 <31945378+mrm80612@users.noreply.github.com>
2017-10-11 20:06:25 -0700

    updates

--- views/results.php
+++ views/results.php
@@ -1,9 +1,9 @@
 <?php include 'partials/header.php'; ?>
-<!-- <pre>
-  <?php //print_r($forecast); ?>
-</pre> -->
+<pre>
+  <?php print_r($forecast); ?>
+</pre>
 <main>
-      <div class="jumbotron jumbotron-fluid" style="background: url('images/01-winter.jpg') center center no-repeat !important; background-size: cover !important;">
+      <div class="jumbotron jumbotron-fluid" style="background: url('images/03-summer.jpg') center center no-repeat !important; background-size: cover !important;">
         <div class="d-flex justify-content-between">
           <div class="card p-4" style="max-width: 240px; opacity: 0.75;">
             <p class="lead text-bold m-0"><?php echo $place; ?></p>
@@ -12,9 +12,6 @@
             </h2>
             <p class="lead">
               <?php echo $forecast['currently']['summary']; ?>
-            </p>
-            <p class="lead">
-              Wind Speed: <?php echo round($forecast['currently']['windSpeed']); ?> MPH
             </p>
           </div>
 
@@ -31,48 +28,53 @@
           </form>
         </div>
 
-        <div class="container">
-          <div class="card p-4 my-5" style="opacity: 0.5">
-            <div class="row">
-              <?php foreach($forecast['hourly']['data'] as $hours): ?>
-                <p class="lead m-0">
-                  <?php echo gmdate("g A", $hours['time']); ?>
-                </p>
-                <h2 class="m-0">
-                  <?php echo round($hours['temperature']); ?>&deg;
-                </h2>
-              <?php endforeach; ?>
+      <div class="d-flex justify-content-between">
+        <div class="flex-column">
+          <div class="container">
+            <div class="card p-4 my-5" style="opacity: 0.5">
+              <div class="row">
+                <?php foreach($forecast['hourly']['data'] as $hours): ?>
+                  <p class="lead m-0">
+                    <?php echo gmdate("g A", $hours['time']); ?>
+                  </p>
+                  <h2 class="lead m-0">
+                    <?php echo round($hours['temperature']); ?>&deg;
+                  </h2>
+                <?php endforeach; ?>
+              </div>
+            </div>
+          </div>
+          <div class="container">
+            <div class="row justify-content-start" style="opacity: 0.5">
+              <?php foreach($forecast['daily']['data'] as $day): ?>
+                <div class="card p-4 my-5">
+                  <p class="lead m-0">
+                    <?php echo gmdate("D", $day['time']); ?>
+                  </p>
+                  <h2 class="lead m-0">
+                    <?php echo round($day['temperatureHigh']); ?>&deg;
+                  </h2>
+                </div>
+              <?php endforeach; ?> 
             </div>
           </div>
         </div>
 
-      <div class="d-flex justify-content-between">
-        <div class="container">
-          <div class="row justify-content-start" style="opacity: 0.5">
-            <?php foreach($forecast['daily']['data'] as $day): ?>
-              <div class="card p-4 my-5">
-                <p class="lead m-0">
-                  <?php echo gmdate("D", $day['time']); ?>
-                </p>
-                <h2 class="m-0">
-                  <?php echo round($day['temperatureHigh']); ?>&deg;
-                </h2>
-              </div>
-            <?php endforeach; ?> 
-          </div>
-        </div>
         <div class="container">
           <div class="card p-4 my-5" style="opacity: 0.5">
-            <div class="row">
-              <?php foreach($forecast['hourly']['data'] as $hours): ?>
-                <p class="lead m-0">
-                  <?php echo gmdate("g A", $hours['time']); ?>
-                </p>
-                <h2 class="m-0">
-                  <?php echo round($hours['temperature']); ?>&deg;
-                </h2>
-              <?php endforeach; ?>
-            </div>
+              <p class="lead text-bold m-0">Details</p>
+              <p class="lead">
+                Temperature: <?php echo round($forecast['currently']['temperature']); ?>&deg;
+              </p>
+              <p class="lead">
+                Humidity: <?php echo round($forecast['currently']['humidity']); ?> %
+              </p>
+              <p class="lead">
+                Visibility: <?php echo round($forecast['currently']['visibility']); ?> Miles
+              </p>
+              <p class="lead">
+                UV Index: <?php echo round($forecast['currently']['uvIndex']); ?>
+              </p>
           </div>
          </div>
         </div>
